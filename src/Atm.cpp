947e86a282805adc70a6dd2732c93457896a4ce1
faisal <mohd.faisal@blubook.co.in>
2024-11-18 18:23:33 +0530

    added all functionalities

--- src/Atm.cpp
+++ src/Atm.cpp
@@ -13,7 +13,49 @@ void ATM::showMenu(){
     std::cout<<"Enter Your Choice!!";
 }
 void handleTransactiions(){
+    int choice;
 
+    do{
+        showMenu();
+        std::cin>>choice;
+        switch(choice){
+            case 1:
+            std::cout<<"Balance:"<<bank_.getBalance(loggedInAcccount_)<<"\n";
+            break;
+        }
+        case 2:
+    {
+        double amount;
+        std::cout<<"Enter the amount to deposit"<<endl;
+        std::cin>>amount;
+        bank_.deposit(loggedInAccount_,amount);
+        std::cout<<"Amount Deposited succesfully!!!";
+        break;
+
+    }
+    case 3:
+    {      double amount;
+        std::cout<<"Enter the amount to withdraw!!";
+        if(bank_.withdraw(loggedInAccount_,amount)){
+            std::cout<<"Withdrawal successful!!\n";
+
+        }
+        else{
+            std::cout<<"Insufficient Balance!!";
+
+        }
+        break;
+    }
+    case 4:
+    std::cout<<"loggin out...\n";
+    loggedInAcount_="";
+    return;
+    default:
+    std::cerr<<"Invalid Choice!!!!\n";
+
+
+    }
+    while(true);
 }
 void handleLogin(){
     std::string accountNumber,pin;
@@ -21,5 +63,13 @@ void handleLogin(){
     std::cin>>accountNumber;
     std::cout<<"Enter Pin: ";
     std::cin>>pin;
+    if(bank_.authenticate(accountNumber,pin)){
+        loggedInAccount_= accountNumber;
+        std::cout<<"Login Successfull!!";
+    }else{
+        std::cerr<<"Invalid Credentials. Exiting...\n";
+        exit(1);
+    }
           
 }
+

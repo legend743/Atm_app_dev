c7835505756936d4a6a5aa1114e2b6e6240438b7
faisal <mohd.faisal@blubook.co.in>
2024-11-19 10:52:32 +0530

    method implemented

---
+++ src/Bank.cpp
@@ -0,0 +1,24 @@
+#include "Bank.hpp"
+
+Bank::Bank(){
+//Adding dummy accounts for demonstrations
+accounts_.emplace("758100046690",Account("758100046690","0000",1000.0));
+accounts_.emplace("758100046690",Account("758100056690","9999",11000.0));
+}
+bool Bank::authenticate(const std::string &accountNumber,cost std::string &pin){
+    if(accounts_.find(accountNumber)!=accounts_.end()){
+        return accounts_[accountNumber].validatePin(pin);
+
+    }
+    return false;
+}
+double Bank::getBalance(const std::string &accountNumber){
+    return accounts_[accountNumber].getBalance();
+
+}
+void Bank::deposit(const std::string &accountNumber,double amount){
+    accounts_[accountNumber].deposit(amount);
+}
+bool Bank::withdraw(const std::string &accountNumber,double amount){
+    return accounts_.[accountNumber].withdraw(amount)
+}

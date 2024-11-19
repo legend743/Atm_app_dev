b096351cd544abf3fa13eab7adf513036d8f03fe
faisal <mohd.faisal@blubook.co.in>
2024-11-19 10:49:35 +0530

    account code added

---
+++ include/Account.hpp
@@ -0,0 +1,16 @@
+#ifndef ACCOUNT_HPP
+#define ACCOUNT_HPP
+#include<string>
+class Account{
+    public:
+    Account(const std::string &accountNumber,const std::string &pin,double balance);
+    bool validatePin(const std::string &pin) const;
+    double getBalance() const;
+    void deposit (double amount);
+    bool withdraw(double amount);
+    private:
+    std::string accountNumber_;
+    std::string pin_;
+    double balance_;
+};
+#endif

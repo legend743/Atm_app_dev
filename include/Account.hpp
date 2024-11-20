2bac6aa61fd7ea62364dc3239adbed4cb99a0a09
faisal <mohd.faisal@blubook.co.in>
2024-11-20 09:47:52 +0530

    major bugs solved

--- include/Account.hpp
+++ include/Account.hpp
@@ -2,7 +2,11 @@
 #define ACCOUNT_HPP
 #include<string>
 class Account{
+
+
     public:
+
+    Account();      //default constructor
     Account(const std::string &accountNumber,const std::string &pin,double balance);
     bool validatePin(const std::string &pin) const;
     double getBalance() const;
